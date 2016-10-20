4ea7d7f18b691eb73b24678030c4e38038f91690
ChazalFlorian <chazal.florian@gmail.com>
2016-10-20 16:18:52 +0200

    [Features] added model Clients (debug purpose only)

--- API/models/client.py
+++ API/models/client.py
@@ -1,29 +1,9 @@
 from enum import Enum
 
+
 class Client:
-    name = String()
-    entreprise = String()
     state = Enum('Prospect', 'Projet en cours', 'Projet Terminé', 'Partenaire')
 
     def init(self, name, entreprise, state):
         self.name = name
         self.entreprise = entreprise
-        self.state = state
-
-    def getName():
-        return self.name
-
-    def getEntreprise():
-        return self.entreprise
-
-    def getState():
-        return self.state
-
-    def setName(name):
-        self.name = name
-
-    def setEntreprise(entreprise):
-        self.entreprise = entreprise
-
-    def setState(state):
-        self.state = state
