8eae1f1c05d03f61bad4ca758a53657d7634c355
Clément Dufour <clement.dufour@supinternet.fr>
2016-10-24 14:20:20 +0200

    [FEATURE] Added routes and controllers to get one/all, add, edit and remove clients

--- API/controllers/client.py
+++ API/controllers/client.py
@@ -1,14 +1,134 @@
+from flask import request
+from json import dumps
+
+from API.models.clients import Clients
+from API.models.client import State
+
+
 def get(id):
-    return 'get {}'.format(id)
+    clients = Clients()
+
+    for client in clients.container:
+        if client.id == id:
+            return dumps({
+                "success": True,
+                "client": {
+                    "id": client.id,
+                    "name": client.name,
+                    "company": client.company,
+                    "state": client.state.value
+                }
+            }, indent=4)
+
+    return dumps({
+        "success": False,
+        "message": "Client not found"
+    }, indent=4)
 
 
+def getAll():
+    clients = Clients()
+    clientsList = []
+
+    for client in clients.container:
+            clientsList.append({
+                "id": client.id,
+                "name": client.name,
+                "company": client.company,
+                "state": client.state.value
+            })
+
+    return dumps(clientsList, indent=4)
+
+
+# Post parameters :
+#    name (string)
+#    company (string)
+#    state ('prospect' | 'inProgress' | 'finished' | 'partner')
+#
 def add():
-    return 'add'
+    if (
+        'name' in request.form
+        and 'company' in request.form
+        and 'state' in request.form
+        and request.form['state'] in State.__members__
+    ):
+        clients = Clients()
+        client = clients.addClient(
+            request.form['name'],
+            request.form['company'],
+            request.form['state']
+        )
+
+        return dumps({
+            "success": True,
+            "client": {
+                "id": client.id,
+                "name": client.name,
+                "company": client.company,
+                "state": client.state.value
+            }
+        }, indent=4)
+    else:
+        return dumps({
+            "success": False,
+            "message": "Missing parameters or wrong state"
+        }, indent=4)
 
 
+# Post parameters :
+#    name (string)
+#    company (string)
+#    state ('prospect' | 'inProgress' | 'finished' | 'partner')
+#
 def edit(id):
-    return 'edit'
+    if (
+        'name' in request.form
+        and 'company' in request.form
+        and 'state' in request.form
+        and request.form['state'] in State.__members__
+    ):
+        clients = Clients()
+
+        for client in clients.container:
+            if client.id == id:
+                client.name = request.form['name']
+                client.company = request.form['company']
+                client.state = State[request.form['state']]
+
+                return dumps({
+                    "success": True,
+                    "client": {
+                        "id": client.id,
+                        "name": client.name,
+                        "company": client.company,
+                        "state": client.state.value
+                    }
+                }, indent=4)
+
+        return dumps({
+            "success": False,
+            "message": "Client not found"
+        }, indent=4)
+    else:
+        return dumps({
+            "success": False,
+            "message": "Missing parameters or wrong state"
+        }, indent=4)
 
 
 def delete(id):
-    return 'delete'
+    clients = Clients()
+
+    for client in clients.container:
+        if client.id == id:
+            clients.container.remove(client)
+
+            return dumps({
+                "success": True
+            }, indent=4)
+
+    return dumps({
+        "success": False,
+        "message": "Client not found"
+    }, indent=4)
--- API/routes/client.py
+++ API/routes/client.py
@@ -2,10 +2,37 @@ from API import app
 from API.controllers import client
 
 
-app.add_url_rule('/client/get/<int:id>', 'getClient', client.get, methods=["GET"])
+app.add_url_rule(
+    '/client/get/<int:id>',
+    'getClient',
+    client.get,
+    methods=["GET"]
+)
 
-app.add_url_rule('/client/add', 'addClient', client.add, methods=["GET"])
+app.add_url_rule(
+    '/client/get',
+    'getClients',
+    client.getAll,
+    methods=["GET"]
+)
 
-app.add_url_rule('/client/edit/<int:id>', 'editClient', client.edit, methods=["GET"])
+app.add_url_rule(
+    '/client/add',
+    'addClient',
+    client.add,
+    methods=["POST"]
+)
 
-app.add_url_rule('/client/delete/<int:id>', 'deleteClient', client.delete, methods=["GET"])
+app.add_url_rule(
+    '/client/edit/<int:id>',
+    'editClient',
+    client.edit,
+    methods=["POST"]
+)
+
+app.add_url_rule(
+    '/client/delete/<int:id>',
+    'deleteClient',
+    client.delete,
+    methods=["GET"]
+)
