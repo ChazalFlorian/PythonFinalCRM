e2a92ca635f62f75d5696eede1570cf5915c6531
Clément Dufour <clement.dufour@supinternet.fr>
2016-11-01 21:06:01 +0100

    [FEATURE] Added a MySQL database support with SQLAlchemy

--- API/__init__.py
+++ API/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://eiron@localhost/pythoncrm'
+db = SQLAlchemy(app)
 
 import API.routes.client
