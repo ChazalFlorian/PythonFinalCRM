193dae762d1daf6a172f92938a5689e07107437e
Clément Dufour <clement.dufour@supinternet.fr>
2016-11-02 17:52:38 +0100

    [FIX] Moved db username and password to config/db.json

--- API/__init__.py
+++ API/__init__.py
@@ -1,8 +1,14 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from json import load
+
+
+with open(os.path.dirname(os.path.abspath(__file__))+"/config/db.json") as dbconfig:
+    data = load(dbconfig)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://eiron@localhost/pythoncrm'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@localhost/pythoncrm'.format(data['username'], data['password'])
 db = SQLAlchemy(app)
 
 import API.routes.client
